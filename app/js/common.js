f8594c558a1ab427c1677b3f2885717d0de05869
Игорь Кирилюк <Barraki201@gmail.com>
2019-02-08 02:55:45 +0200

    Add Header and first section

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,17 @@
 $(function() {
 
-	// Custom JS
+	$(".slider-home").slick({
+		dots: true,
+		adaptiveHeight: true,
+		arrows: false,
+		responsive: [{
+			breakpoint: 1200,
+			settings: {
+				centerMode: true,
+				slidesToShow: 1,
+				variableWidth: true
+			}
+		}]
+	});
 
 });
