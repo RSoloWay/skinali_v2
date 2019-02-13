d7636366abe450ac62134ce2dcec6dbf18a30cbc
Игорь Кирилюк <Barraki201@gmail.com>
2019-02-13 04:29:52 +0200

    add adaptive

--- app/js/common.js
+++ app/js/common.js
@@ -1,62 +1,84 @@
-$(function() {
-  $("#slider-home").slick({
-    adaptiveHeight: true,
-    arrows: true,
-    // fade: true,
-    appendArrows: '#first-screen .container',
-    responsive: [
-      {
-        breakpoint: 1200,
-        settings: {
-          centerMode: true,
-          variableWidth: true
-        }
-      }
-    ]
-  });
+$(function () {
+	$("#slider-home").slick({
+		adaptiveHeight: true,
+		arrows: true,
+		// fade: true,
+		appendArrows: '#first-screen .container',
+		responsive: [{
+			breakpoint: 1200,
+			settings: {
+				centerMode: true,
+				variableWidth: true,
+				appendArrows: '',
+			},
+			breakpoint: 786,
+			settings: {
+				centerMode: false,
+				variableWidth: false,
+				appendArrows: '',
+			}
+		}]
+	});
 
-  $(document).ready(function() {
-    var el = "#first-screen #slider-home, #first-screen .container";
-    var heigth;
-    var widht = $(window).width();
+	$(document).ready(function () {
+		var el = "#first-screen #slider-home, #first-screen .container";
+		var heigth;
+		var widht = $(window).width();
 
-    heigth = widht * 0.305;
+		heigth = widht * 0.305;
 
-    if ($(window).width() <= 1200) {
-    return
-    }
+		if ($(window).width() <= 1200) {
+			heigth = $("#slider-home img").height()
+		}
 
-    $(el).css("height", heigth);
-    console.log("Print");
-    
-  });
+		if ($(window).width() <= 768) {
+			heigth = "100%"
+		}
 
-  $(document).ready(function() {
-    var el = ".call-to-action";
-    var margin;
-    var scale;
-    var widht = $(window).width();
 
-    scale = widht / 1700;
-    margin = widht / 30;
+		$(el).css("height", heigth);
+	});
 
-    if ($(window).width() <= 1600 && $(window).width() >= 1200) {
+	$(document).ready(function () {
+		var el = ".call-to-action";
+		var margin;
+		var scale;
+		var widht = $(window).width();
 
-      $(el).css("transform", "scale(" + scale + ")");
-      $(el).css("margin-top", margin);
-    } else {
-    }
+		scale = widht / 1700;
+		margin = widht / 30;
 
-    $(el).animate(
-      {
-        opacity: 1
-      },
-      1000,
-      function() {
-        // Animation complete.
-      }
-    );
+		if ($(window).width() <= 1600 && $(window).width() >= 1200) {
 
-    return;
-  });
+			$(el).css("transform", "scale(" + scale + ")");
+			$(el).css("margin-top", margin);
+		} else {}
+
+		if ($(window).width() >= 768) {
+			$(el).animate({
+					opacity: 1
+				},
+				1000,
+				function () {}
+			);
+		} else {}
+
+
+
+		return;
+	});
+
+
+	$(document).ready(function () {
+		$(".menu-btn").click(function () {
+			$("#mobile-menu").fadeIn("1000");
+			$(".menu").css("right", "0");
+			$("body").css("overflow", "hidden");
+		})
+		$(".icon-close").click(function () {
+			$(".menu").css("right", "-100px");
+			$("#mobile-menu").fadeOut("1000");
+			$("body").css("overflow", "scroll");
+		})
+	})
 });
